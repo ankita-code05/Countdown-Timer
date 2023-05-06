981d0999142f2629d02c9adc1650fd32b8b756a4
Ankita Tiwari <ankitatiwari12001@gmail.com>
2023-05-06 14:43:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+    this.intervalId = null;
+  }
+
+  handleKeyDown(event) {
+    if (event.key === "Enter") {
+      const input = event.target.value;
+      const time = Math.floor(Number(input));
+      if (!isNaN(time) && time >= 0) {
+        if (this.intervalId) {
+          clearInterval(this.intervalId);
+        }
+        this.setState({ time });
+        this.intervalId = setInterval(() => {
+          this.setState((prevState) => ({
+            time: prevState.time - 1
+          }), () => {
+            if (this.state.time === 0) {
+              clearInterval(this.intervalId);
+            }
+          });
+        }, 1000);
+      } else {
+        this.setState({ time: 0 });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Countdown Timer</h1>
+        <label htmlFor="timer">Set Timer</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter time in seconds"
+          onKeyDown={(event) => this.handleKeyDown(event)}
+        />
+        <div id="current-time">{this.state.time}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
